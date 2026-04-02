3004f3f3779009a754b2d77884c8ec70475188bd
Gustavo-Montero <gmonterog@ucenfotec.ac.cr>
2026-04-01 21:53:00 -0600

    avance

--- Proyecto.Tests/obj/Debug/net10.0/Proyecto.Tests.AssemblyInfo.cs
+++ Proyecto.Tests/obj/Debug/net10.0/Proyecto.Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Proyecto.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0a59172c3929f09216cffcbad07861af5113821")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b12f4f5b256d01ec5aea46cfc92f6c2247b0a414")]
 [assembly: System.Reflection.AssemblyProductAttribute("Proyecto.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Proyecto.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
